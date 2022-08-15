b427d957dae65dbe1dd994aa707229120087fc79
Jeff <odhiambo47@gmail.com>
2022-08-15 22:47:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+//const ul = document.getElementById("main")
+//ul.removeChild("main")
+ main.remove()
+
+ const newHeader = document.createElement('h1')
+ newHeader.id = 'victory'
+ newHeader.innerHTML = 'YOUR-NAME is the champion'
